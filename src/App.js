5c16ec8cfe522a5a99ee6030b842a9ade7a0e940
divizia21 <divizia21@bk.ru>
2022-09-05 19:33:01 +0300

    add BrouserRouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React, {useEffect} from "react";
+import React from "react";
 import './App.css';
 import Header from "./components/Header/Header";
 import {TodoMainContainer} from "./components/TodoMain/TodoMainContainer";
-
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
 
 
 function App() {
   return (
-    <>
-        <div className='container'>
-            <Header/>
-            <TodoMainContainer/>
-        </div>
-    </>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={
+                <div className='container'>
+                <Header/>
+                <TodoMainContainer/>
+            </div>
+            }/>
+        </Routes>
+    </BrowserRouter>
   );
 }
 
